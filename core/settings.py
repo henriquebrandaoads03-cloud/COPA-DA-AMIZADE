7eda557a5aa67c6299d8e49616cf8b2eda442488
Rodrigo <detonadorrodrigo11@gmail.com>
2025-12-15 17:01:28 +0000

    add STATICFILES_DIRS = [     os.path.join(BASE_DIR, static)resolved the bug static

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
